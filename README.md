49a8b468f22d20403fb24bf685fcb2f8e9cf9a4b
Kamran Hussain(UST <308674@ust.com>
2026-05-19 13:38:04 +0530

    added s3 terraform file

---
+++ README.md
@@ -0,0 +1,1 @@
+"# brownfield-repo-small-1" 
---
+++ s3.tf
@@ -0,0 +1,23 @@
+resource "aws_s3_bucket" "main" {
+  bucket = "poc-bucket-${random_id.suffix.hex}"
+
+  tags = { Name = "poc-main-bucket" }
+}
+
+resource "aws_s3_bucket_versioning" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "main" {
+  bucket = aws_s3_bucket.main.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
